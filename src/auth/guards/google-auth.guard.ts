b51ceb51023f26c5b64feb988cbbe4c61450ecb9
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2020-09-10 02:02:00 +0300

    [auth] - handling google auth errors

--- src/auth/guards/google-auth.guard.ts
+++ src/auth/guards/google-auth.guard.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
 export class GoogleAuthGuard extends AuthGuard('google') {
+	handleRequest(err, user) {
+		if (err || !user) {
+			throw new HttpException(err.message, HttpStatus.FORBIDDEN);
+		}
 
+		return user;
+	}
 }

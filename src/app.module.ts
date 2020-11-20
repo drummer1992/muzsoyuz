d3d8fc8cbe446e20afaba47dbb8ae85791ca6ec4
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2020-11-20 23:43:46 +0200

    - rollback

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { config } from './config'
 import { AuthModule } from './modules/auth/auth.module'
 import { UserModule } from './modules/users/user.module'
 import { JobModule } from './modules/job/job.module'
-import { AppController } from './app.controller'
 
 @Module({
 	imports: [
@@ -15,7 +14,6 @@ import { AppController } from './app.controller'
 		JobModule,
 		AuthModule,
 	],
-	controllers: [AppController]
 })
 
 export class AppModule {

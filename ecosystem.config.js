e8f1895b8448a22f75b9e8019746d5117c6325e5
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2020-11-29 18:47:00 +0200

    - removed ci

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,24 +2,5 @@ module.exports = {
   apps : [{
     name: 'muzsoyuz',
     script: 'npm run start:prod',
-  }],
-  deploy: {
-    production : {
-      // SSH user
-      user : 'root',
-      // SSH host
-      host : '165.227.137.68',
-      key: 'deploy.key',
-      // GIT remote/branch
-      ref  : 'origin/master',
-      // GIT remote
-      repo : 'https://github.com/drummer1992/muzsoyuz',
-      // Fetch all branches or fast
-      fetch: 'all',
-      // Path in the server
-      path : '/home/nodejs/muzsoyuz',
-      // Command run after pull source code
-      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js && pm2 save'
-    }
-  }
+  }]
 };

48c42337355284858d86ea7a1320801d3c94b17a
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2020-11-29 14:53:53 +0200

    - ci setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,4 +3,23 @@ module.exports = {
     name: 'muzsoyuz',
     script: 'npm run start:prod',
   }],
+  deploy: {
+    production : {
+      // SSH user
+      user : 'root',
+      // SSH host
+      host : '165.227.137.68',
+      key: 'deploy.key',
+      // GIT remote/branch
+      ref  : 'origin/master',
+      // GIT remote
+      repo : 'github.com/drummer1992/muzsoyuz',
+      // Fetch all branches or fast
+      fetch: 'all',
+      // Path in the server
+      path : '/home/nodejs/muzsoyuz',
+      // Command run after pull source code
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js'
+    }
+  }
 };
